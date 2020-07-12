b161267419b9b9a2a510aa81903bf25ae9614135
Jéssica Leal <62772815+Jessy777-cripto@users.noreply.github.com>
2020-07-12 19:41:39 -0300

    completed

--- Desafios Python/ex0056.py
+++ Desafios Python/ex0056.py
@@ -1,16 +1,23 @@
-p = int(input('Quantas pessoas deseja analisar? '))
-x = 0
-for a in range(1, p+1):
+somaidade = 0
+homemmaisvelho = 0
+media = 0
+nomedovelho = ''
+totmulher = 0
+for a in range(1, 5):
     print('---- {}° PESSOA ----'.format(a))
     nome = str(input('Nome: '))
-    idade = int(input('Idade: '))
-    sexo = str(input('Sexo [M/F]: ')).upper()
-    x += idade
-    y = 0
-    if sexo == 'M' and idade > y :
-        y = idade
-        homenmaisvelho = nome
-media = x/p
-print(nome)
-print(media)
-print(x)
+    idade = float(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: '))
+    somaidade += idade
+    if a == 1 and sexo in 'Mm':
+        homemmaisvelho = idade
+        nomedovelho = nome
+    if sexo in 'Mm' and idade > homemmaisvelho:
+        homemmaisvelho = idade
+        nomedovelho = nome
+    if sexo in 'Ff' and idade < 20:
+        totmulher +=1
+media = somaidade / 4
+print('O nome do homem mais velho do grupo é {}.'.format(nome))
+print('A média de idade dos participantes do grupo é de {:.2f}'.format(media))
+print('O número de mulheres que possuem menos de 20 anos é de {}.'.format(totmulher))
---
+++ Python Mundo 2/aulas/aula14a.py
@@ -0,0 +1,30 @@
+#programas da aula 14
+#14 class softwares
+
+# for c in range(1, 10):
+#     print(c)
+# print('Fim')
+
+# par = 0
+# impar = 0
+# n = 1
+# while n!= 0:
+#     n = int(input('Digite um valor: '))
+#     if n % 2 == 0:
+#         par += 1
+#     else: 
+#         impar += 1
+# print(par)
+# print(impar)
+# print('Fim')
+
+n = 'S'
+while n == 'S':
+    p = int(input('Digite um valor: '))
+    n = str(input('Quer continuar? [S/N] ')).upper()
+print('Fim')
+
+# c = 1
+# while c < 10:
+#     print(c)
+#     c = c + 1
