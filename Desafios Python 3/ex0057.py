4eeb93929619c9a8a76e99aaf8ce6313312d5e36
Jéssica Leal <62772815+Jessy777-cripto@users.noreply.github.com>
2020-07-18 16:35:10 -0300

    Update ex0057.py

--- Desafios Python 3/ex0057.py
+++ Desafios Python 3/ex0057.py
@@ -2,7 +2,7 @@ a = 'S'
 while a == 'S':
     sex = str(input('Insira qual o seu gênero [F/M/NB]: ')).upper().strip()
     # F é feminíno, M é masculino, NB é não binário
-    if sex != 'F' or sex != 'M' or sex != 'NB':
+    if sex != 'F' and sex != 'M' and sex != 'NB':
         a = str(input('Dígito errado, deseja continuar do início? [S/N] ')).upper()
 print('Fim')
     # if sexo != 'F' or sexo != 'M'
