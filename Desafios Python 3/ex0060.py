2a5adf3bb6f1d1991a5aa33cae9f3724e7cc067b
Jéssica Leal <62772815+Jessy777-cripto@users.noreply.github.com>
2020-07-19 19:08:26 -0300

    delete

--- Desafios Python 3/ex0060.py
+++
@@ -1,8 +0,0 @@
-n = int(input('Digite um número para calcularmos o seu fatorial: '))
-p = 1
-while n > 0:
-    print(n)
-    n = n-1
-    y = 
-print(y)
-    
--- Desafios Python 3/ex0062.py
+++
@@ -1,9 +0,0 @@
-x = int(input('Quantos termos deseja que mostre na tela? '))
-no = int(input('Digite o primeiro termo de uma PA: '))
-razao = int(input('Digite a razão dessa PA: '))
-q = no
-while x != 0:
-    print('{}'.format(q), end= '.. ')   
-    q += razao
-    x = x - 1 
-print('ACABOU..', end=' ')
